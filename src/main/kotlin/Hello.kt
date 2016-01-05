15694f0ceb615e0760a48673dbaedc147fefb91e
zoetrope <a.ikezoe@gmail.com>
2016-01-06 01:44:33 +0900

    add Kovenant

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,12 +1,20 @@
+import nl.komponents.kovenant.combine.and
+import nl.komponents.kovenant.task
 
 fun main(args: Array<String>) {
     val h = Hello();
-    h.hello()
+    h.asyncHello()
 }
 
 class Hello {
     fun hello() {
         println("Hello World!");
+    }
+
+    fun asyncHello(){
+        task { "world" } and task { "Hello" } success {
+            println("${it.second} ${it.first}?")
+        }
     }
 
     fun calc(a: Int, b: Int): Int = a + b
