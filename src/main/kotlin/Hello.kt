6b32b6eea0b8f2e0efec0525cb60f6b216c438de
zoetrope <a.ikezoe@gmail.com>
2016-01-06 19:29:00 +0900

    add sample for nested promise

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,9 +1,14 @@
 import nl.komponents.kovenant.combine.and
+import nl.komponents.kovenant.functional.bind
+import nl.komponents.kovenant.functional.map
 import nl.komponents.kovenant.task
+import nl.komponents.kovenant.then
+import nl.komponents.kovenant.thenUse
 
 fun main(args: Array<String>) {
     val h = Hello();
-    h.asyncHello()
+    //    h.asyncHello()
+    h.nestedPromise()
 }
 
 class Hello {
@@ -11,10 +16,38 @@ class Hello {
         println("Hello World!");
     }
 
-    fun asyncHello(){
+    fun asyncHello() {
         task { "world" } and task { "Hello" } success {
             println("${it.second} ${it.first}?")
         }
+    }
+
+    fun nestedPromise() {
+        val p1 = task {
+            Thread.sleep(2000)
+            "Hello"
+        }
+        val p2 = task {
+            Thread.sleep(1000)
+            "World"
+        }
+
+        p1 bind {
+            println(it)
+            p2
+        } success {
+            println(it)
+        }
+
+        p1 bind {
+            p2 thenUse {
+                it to this
+            }
+        } success {
+            println(it)
+        }
+
+        println("finish")
     }
 
     fun calc(a: Int, b: Int): Int = a + b
