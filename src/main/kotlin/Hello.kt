5fb7a0a5b1a8927f07130e8430b92debe75016d4
zoetrope <a.ikezoe@gmail.com>
2016-01-06 19:57:29 +0900

    add sample for rx

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,14 +1,19 @@
+import nl.komponents.kovenant.*
 import nl.komponents.kovenant.combine.and
 import nl.komponents.kovenant.functional.bind
 import nl.komponents.kovenant.functional.map
-import nl.komponents.kovenant.task
-import nl.komponents.kovenant.then
-import nl.komponents.kovenant.thenUse
+import rx.Observable
+import rx.Subscriber
+import rx.lang.kotlin.AsyncSubject
+import rx.lang.kotlin.deferredObservable
+import rx.lang.kotlin.fold
+import rx.lang.kotlin.observable
 
 fun main(args: Array<String>) {
     val h = Hello();
     //    h.asyncHello()
-    h.nestedPromise()
+    //h.obs()
+    h.promiseToObservable()
 }
 
 class Hello {
@@ -51,4 +56,60 @@ class Hello {
     }
 
     fun calc(a: Int, b: Int): Int = a + b
+
+    fun <T> Observable<T>.toPromise(): Promise<T, Throwable> {
+        val deferred = deferred<T, Throwable>();
+        this.subscribe({ x -> deferred.resolve(x) }, { e -> deferred.reject(e) })
+        return deferred.promise;
+    }
+
+    fun <V> Promise<V, Throwable>.toObservable(): Observable<V> {
+        val subject = AsyncSubject<V>()
+        success {
+            subject.onNext(it)
+            subject.onCompleted()
+        } fail {
+            subject.onError(it)
+        }
+        return subject;
+    }
+
+    fun <T> observable2(body: Subscriber<in T>.() -> Unit): Observable<T> = Observable.create(body)
+
+    fun obs() {
+        observable2<String> {
+            onNext("H")
+            onNext("e")
+            onNext("l")
+            onNext("")
+            onNext("l")
+            onNext("o")
+            onCompleted()
+        }
+                .filter { it.isNotEmpty() }
+                .fold (StringBuilder()) { sb, e -> sb.append(e) }
+                .map { it.toString() }
+                .subscribe { result ->
+                    print(result)
+                }
+
+    }
+
+    fun obsToPromise() {
+        observable<String> { subscriber ->
+            subscriber.onNext("Observable to Promise")
+            subscriber.onCompleted()
+        }.toPromise() success{
+            println(it)
+        }
+    }
+
+    fun promiseToObservable() {
+        task {
+            "Promise to Observable"
+        }.toObservable().subscribe({
+            println(it)
+        })
+    }
+
 }
