d82fd043ef5178232b277df86407226f4b5faf60
zoetrope <a.ikezoe@gmail.com>
2016-01-06 23:34:11 +0900

    add elasticsearch sample

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -2,18 +2,29 @@ import nl.komponents.kovenant.*
 import nl.komponents.kovenant.combine.and
 import nl.komponents.kovenant.functional.bind
 import nl.komponents.kovenant.functional.map
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.index.IndexResponse
+import org.elasticsearch.action.search.SearchRequestBuilder
+import org.elasticsearch.action.search.SearchResponse
+import org.elasticsearch.client.transport.TransportClient
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.transport.InetSocketTransportAddress
+import org.elasticsearch.common.transport.TransportAddress
+import org.elasticsearch.index.query.QueryBuilders
 import rx.Observable
 import rx.Subscriber
 import rx.lang.kotlin.AsyncSubject
 import rx.lang.kotlin.deferredObservable
 import rx.lang.kotlin.fold
 import rx.lang.kotlin.observable
+import java.net.InetAddress
 
 fun main(args: Array<String>) {
     val h = Hello();
     //    h.asyncHello()
     //h.obs()
-    h.promiseToObservable()
+    //h.promiseToObservable()
+    h.es()
 }
 
 class Hello {
@@ -110,6 +121,59 @@ class Hello {
         }.toObservable().subscribe({
             println(it)
         })
+    }
+
+    fun es() {
+        val client = esClient(listOf(InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300))) {
+            settings(esSettings {
+                put("cluster.name", "elasticsearch")
+            })
+        }
+
+        client.searchAsync("*") {
+            setSize(10)
+            setQuery(QueryBuilders.matchAllQuery())
+        } success {
+            it.hits?.hits?.forEach {
+                println(it.source)
+            }
+        }
+        Thread.sleep(5000)
+    }
+
+    fun esSettings(block: Settings.Builder.() -> Unit): Settings {
+        val settings = Settings.settingsBuilder()
+        settings.block()
+        return settings.build()
+    }
+
+    fun esClient(nodes: List<TransportAddress>, block: TransportClient.Builder.() -> Unit): TransportClient {
+        val builder = TransportClient.builder()
+        builder.block()
+        val client = builder.build()
+        nodes.forEach {
+            client.addTransportAddress(it)
+        }
+        return client
+    }
+
+    fun TransportClient.searchAsync(vararg indices: String, block: SearchRequestBuilder.() -> Unit): Promise<SearchResponse, Throwable> {
+
+        val deferred = deferred<SearchResponse, Throwable>();
+        val builder = this.prepareSearch(*indices)
+        builder.block()
+
+        builder.execute(object : ActionListener<SearchResponse> {
+            override fun onResponse(res: SearchResponse) {
+                deferred.resolve(res)
+            }
+
+            override fun onFailure(e: Throwable) {
+                deferred.reject(e)
+            }
+        })
+
+        return deferred.promise
     }
 
 }
